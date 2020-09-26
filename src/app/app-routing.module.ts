50ec88d5384dd8e68db0d8cba6fe4c7917e97843
MO7S3N <34940993+MO7S3N@users.noreply.github.com>
2020-09-26 15:38:55 +0100

    first checkpoint routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CvComponent } from './cv/cv.component';
+import { TodoComponent } from './todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "cv",
+    component : CvComponent
+  }
+  ,
+  {
+    path :"todo",
+    component : TodoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
